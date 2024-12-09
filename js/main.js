ba2b806f0124fdb95865047b9bdf190bdd022ac8
VladpressF <vselvesuk@gmail.com>
2024-12-09 22:12:53 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,46 +1,72 @@
 // 1. Напиши скрипт, який об'єднує всі елементи массива в один рядок. Елементів може бути довільна кількість. Нехай елементи массива в рядку будут розділені комою. 🔽
 // Через for :
 
-// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
-// let string = '';
+const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+let string = '';
 
-// for (let i = 0; i < friends.length; i++) {
-//   string += friends[i];
-//   if (i < friends.length - 1) {
-//     string += ',';
-//   }
-// }
+for (let i = 0; i < friends.length; i++) {
+  string += friends[i];
+  if (i < friends.length - 1) {
+    string += ',';
+  }
+}
 
-// console.log(string); 
+console.log(string); 
 
 // Через join :
 
-// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
-// let string = friends.join(', ');
-// console.log(string)
-
-// 2. Працюємо з колекцією карток в trello. Метод splice() (можна використати інші методи) 🔽
-const cards = [ 
-    'Карточка-1',
-    'Карточка-2',
-    'Карточка-3',
-    'Карточка-4',
-    'Карточка-5'
-
-]
-// 3. Видалити  🔽
-
-cards.splice(0,1);
-console.log(cards);
-
-// 4. Додати 🔽
-
-cards.push('Карточка-6')
-console.log(cards);
-
-// 5. Оновити 🔽
-
-cards.splice(4,1,'Карточка-4')
-console.log(cards)
+const friendsJoin = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+let stringJoin = friendsJoin.join(', ');
+console.log(stringJoin);
 
 
+// 2. Працюємо з колекцією карток в trello. Метод splice() 🔽
+
+const cards = [
+  'Карточка-1',
+  'Карточка-2',
+  'Карточка-3',
+  'Карточка-4',
+  'Карточка-5'
+];
+
+const cardToRemove = 'Карточка-3';
+const cardToInsert = 'Карточка-6';
+const cardToUpdate = 'Карточка-4';
+
+for (let i = 0; i < cards.length; i++) {
+  // Видалити
+  if (cards[i] === cardToRemove) {
+      cards.splice(i, 1);
+      i--; 
+      console.log('Видалено:', cards);
+      continue; 
+  }
+
+  // Додати
+  if (cards[i] === 'Карточка-2' && !cards.includes(cardToInsert)) {
+      cards.splice(i + 1, 0, cardToInsert); 
+      console.log('Додано:', cards);
+      continue;
+  }
+
+  // Оновити
+  if (cards[i] === cardToUpdate) {
+      cards[i] = 'Оновлена-' + cardToUpdate; 
+      console.log('Оновлено:', cards);
+  }
+}
+// // 3. Видалити  🔽
+
+// cards.splice(0,1);
+// console.log(cards);
+
+// // 4. Додати 🔽
+
+// cards.push('Карточка-6')
+// console.log(cards);
+
+// // 5. Оновити 🔽
+
+// cards.splice(4,1,'Карточка-4')
+// console.log(cards)
